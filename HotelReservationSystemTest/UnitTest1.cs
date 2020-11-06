9b1d244c3e954381ed28425530bb344bbd07c07a
Shreya0207 <shanu.shreya270@gmail.com>
2020-11-06 20:16:49 +0530

    [Shreya]Add: Finding the cheapest best rated hotel for reward customers

--- HotelReservationSystemTest/UnitTest1.cs
+++ HotelReservationSystemTest/UnitTest1.cs
@@ -16,35 +16,12 @@ namespace HotelReservationSystemTest
             double expectedRate = 220;
             Assert.AreEqual(expectedRate, hotel.WEEKDAY_RATE);
         }
+
         [Test]
-        public void FindCheapestHotelTest()
+        public void CheapestBestRatedHotelForRewardCustomer_ShouldReturnRidgeWood()
         {
             HotelService service = new HotelService();
-            HotelType hotel = service.FindCheapestHotel("2018-01-01", "2018-01-03");
-            HotelType expected = HotelType.LAKEWOOD;
-            Assert.AreEqual(hotel, expected);
-        }
-        [Test]
-        public void FindCheapestHotelTest_AfterWeekdayAndWeekendRates()
-        {
-            HotelService service = new HotelService();
-            HotelType hotel = service.FindCheapestHotel("2020-09-11", "2020-09-12");
-            HotelType expected = HotelType.LAKEWOOD;
-            Assert.AreEqual(hotel, expected);
-        }
-        [Test]
-        public void CheapestBestRatedHotel_ShouldReturnBridgeWood()
-        {
-            HotelService service = new HotelService();
-            HotelType hotel = service.FindCheapestHotel("2020-09-11", "2020-09-12");
-            HotelType expected = HotelType.BRIDGEWOOD;
-            Assert.AreEqual(hotel, expected);
-        }
-        [Test]
-        public void FindBestRatedHotelTest_ShouldReturnRidgeWood()
-        {
-            HotelService service = new HotelService();
-            HotelType hotel = service.FindBestRatedHotel("2020-09-11", "2020-09-12");
+            HotelType hotel = service.FindCheapestHotel("2020-09-11", "2020-09-12", CustomerType.REWARD);
             HotelType expected = HotelType.RIDGEWOOD;
             Assert.AreEqual(hotel, expected);
         }
